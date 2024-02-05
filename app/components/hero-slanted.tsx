054b96e2818725ce9ce648ea76be61c26657d068
Anthony Stolp <anthony.stolp@gmail.com>
2024-02-05 17:11:10 -0500

    hero style change

--- app/components/hero-slanted.tsx
+++ app/components/hero-slanted.tsx
@@ -2,17 +2,17 @@ export default function HeroSlanted() {
   return (
     <div className="relative flex flex-col lg:flex-col lg:pb-0 lg:mt-12">
       <div className="bg-sSecondary rounded top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-6/12 lg:max-w-full lg:absolute xl:px-0">
-        {/* <svg
+        <svg
           className="text-background absolute left-0 hidden h-full transform -translate-x-1/2 lg:block"
           viewBox="0 0 100 100"
           fill="currentColor"
           preserveAspectRatio="none slice"
         >
           <path d="M50 0H100L50 100H0L50 0Z" />
-        </svg> */}
+        </svg>
         <img
           className="object-cover w-full h-56 rounded h-full"
-          src={'/assets/cup-no-bg-2x-shrunk.png'}
+          src={'/assets/try-hero.png'}
           alt={''}
         />
       </div>

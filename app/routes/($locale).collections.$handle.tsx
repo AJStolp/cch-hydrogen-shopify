f4684e129e523bed9f9cf8b77a3f6ff7ae4288c0
Anthony Stolp <anthony.stolp@gmail.com>
2024-02-14 10:47:47 -0500

    one final grid tweak on mobile, tablet

--- app/routes/($locale).collections.$handle.tsx
+++ app/routes/($locale).collections.$handle.tsx
@@ -65,7 +65,7 @@ export default function Collection() {
 
 function ProductsGrid({products}: {products: ProductItemFragment[]}) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
       {products.map((product, index) => {
         return (
           <ProductItem

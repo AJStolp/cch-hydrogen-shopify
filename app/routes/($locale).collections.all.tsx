573ea53729aa5174e1ef63773937f5d1a729da40
Anthony Stolp <anthony.stolp@gmail.com>
2024-02-14 10:41:34 -0500

    small grid tweak on all products

--- app/routes/($locale).collections.all.tsx
+++ app/routes/($locale).collections.all.tsx
@@ -60,7 +60,7 @@ export default function AllProducts() {
 
 function ProductsGrid({products}: {products: ProductItemFragment[]}) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
       {products.map((product, index) => {
         return (
           <ProductItem

d2da1678eed79f74333dc76058dc1f6000a56076
Anthony Stolp <anthony.stolp@gmail.com>
2024-03-21 11:27:48 -0400

    added sort key

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -265,7 +265,7 @@ const RECOMMENDED_PRODUCTS_QUERY = `#graphql
   }
   query RecommendedProducts ($country: CountryCode, $language: LanguageCode)
     @inContext(country: $country, language: $language) {
-    products(first: 4) {
+    products(first: 4, sortKey: UPDATED_AT) {
       nodes {
         ...RecommendedProduct
       }
